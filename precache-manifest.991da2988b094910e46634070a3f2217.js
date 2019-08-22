b1645fc2f4b3de2f033317ee89f06823ef9689e2
Ishan Shah <ishanshah501@gmail.com>
2019-08-23 00:02:57 +0530

    Updates

---
+++ precache-manifest.991da2988b094910e46634070a3f2217.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7c2e31e005a3fcb0ec48703a504c7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "681181b3023cfab160fa",
+    "url": "/static/css/main.4764afba.chunk.css"
+  },
+  {
+    "revision": "aa076eb92383f5d98e4b",
+    "url": "/static/js/2.c48eca36.chunk.js"
+  },
+  {
+    "revision": "681181b3023cfab160fa",
+    "url": "/static/js/main.c8d8911b.chunk.js"
+  },
+  {
+    "revision": "7fa38cfd15908cefa9be",
+    "url": "/static/js/runtime~main.6cd21c96.js"
+  }
+]);
