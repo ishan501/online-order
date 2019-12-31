8004934266d2f6954db3d534b3d3b800df755c1b
Ishan Shah <ishanshah501@gmail.com>
2019-12-31 22:46:48 +0530

    deploy

---
+++ build/precache-manifest.5f60769deb6471f3110eee202fc38276.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f5e82f7a469e5a27acf05d33da62e0e",
+    "url": "/Swaggy/index.html"
+  },
+  {
+    "revision": "1f2d555f2cafb105c2fd",
+    "url": "/Swaggy/static/css/main.4f27059e.chunk.css"
+  },
+  {
+    "revision": "88030172e244b69f98a6",
+    "url": "/Swaggy/static/js/2.2900cbe3.chunk.js"
+  },
+  {
+    "revision": "1f2d555f2cafb105c2fd",
+    "url": "/Swaggy/static/js/main.02aaa90a.chunk.js"
+  },
+  {
+    "revision": "5e7bab56868dcdb7048b",
+    "url": "/Swaggy/static/js/runtime~main.f96318c9.js"
+  }
+]);
